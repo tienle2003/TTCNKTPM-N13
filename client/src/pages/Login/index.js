30b97d9a6f78c311ba34fb035a5b74169a189df8
truotuan <truongquoctuan1409@gmail.com>
2024-03-16 09:51:28 +0700

    up login and signup page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,3 +1,37 @@
+import classNames from "classnames/bind"
+
+import styles from './Login.module.css'
+import Button from '../../components/Button'
+import routes from '../../constants/routes'
+
+const cx = classNames.bind(styles)
+
 export default function Login() {
-    return <h1>Login page</h1>
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className='w-100'>
+                            <div className="form-box max-w-xl mx-auto px-8 pt-8 pb-24 border rounded-md shadow-md">
+                                <h1 className="text-2xl font-semibold mb-4">Đăng nhập</h1>
+                                <form >
+                                    <div className='flex flex-col mb-4'>
+                                        <label className='text-sm mb' for="phonenumber">SỐ ĐIỆN THOẠI</label>
+                                        <input className='px-2 py-2 outline-none focus:border-colorPrimary' type="number" id="phonenumber" />
+                                    </div>
+                                    <div className='flex flex-col mb-4'>
+                                        <label className='text-sm mb' for="password">MẬT KHẨU</label>
+                                        <input className='px-2 py-2 outline-none focus:border-colorPrimary' type="text" id="password" />
+                                    </div>
+                                    <Button primary className={cx('text-sm font-semibold w-100 hover:underline')}>Đăng nhập</Button>
+                                </form>
+                                <p className='mt-8 text-sm text-right'><Button className={cx('text-colorPrimary hover:text-colorSecondary')} to={routes.signup}>Tạo tài khoản mới</Button></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,3 +1,41 @@
+import classNames from 'classnames/bind'
+
+import styles from './Signup.module.css'
+import Button from '../../components/Button'
+import routes from '../../constants/routes'
+
+const cx = classNames.bind(styles)
+
 export default function Signup() {
-    return <h1>Signup page</h1>
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className='w-100'>
+                            <div className="form-box max-w-xl mx-auto px-8 pt-8 pb-24 border rounded-md shadow-md">
+                                <h1 className="text-2xl font-semibold mb-4">Tạo tài khoản mới</h1>
+                                <form >
+                                    <div className='flex flex-col mb-4'>
+                                        <label className='text-sm mb' for="name">HỌ TÊN</label>
+                                        <input className='px-2 py-2 outline-none border-0 focus:border-colorPrimary' type="text" id="name" />
+                                    </div>
+                                    <div className='flex flex-col mb-4'>
+                                        <label className='text-sm mb' for="phonenumber">SỐ ĐIỆN THOẠI</label>
+                                        <input className='px-2 py-2 outline-none focus:border-colorPrimary' type="number" id="phonenumber" />
+                                    </div>
+                                    <div className='flex flex-col mb-4'>
+                                        <label className='text-sm mb' for="password">TẠO MẬT KHẨU</label>
+                                        <input className='px-2 py-2 outline-none focus:border-colorPrimary' type="text" id="password" />
+                                    </div>
+                                    <Button primary className={cx('text-sm font-semibold w-100 hover:underline')}>Tạo tài khoản</Button>
+                                </form>
+                                <p className='mt-8 text-sm'>Bạn đã có tài khoản? <Button className={cx('text-colorPrimary hover:text-colorSecondary')} to={routes.login}>Đăng nhập ngay</Button></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
